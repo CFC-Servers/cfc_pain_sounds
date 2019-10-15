86d2a285626318e1241613d437b48a6d6ac1fdbd
Bleck <OverlordBleck@gmail.com>
2019-10-14 22:15:52 -0500

    Convert sounds to .wav counterpart

--- lua/autorun/server/sv_pain_sounds.lua
+++ lua/autorun/server/sv_pain_sounds.lua
@@ -1,3 +1,10 @@
+local combineSounds = {
+    "npc/metropolice/pain1.wav",
+    "npc/metropolice/pain2.wav",
+    "npc/metropolice/pain3.wav",
+    "npc/metropolice/pain4.wav"
+}
+
 local HurtSounds = {
     ["cit_male"] = {
         "vo/episode_1/npc/male01/cit_pain01.wav",
@@ -27,60 +34,52 @@ local HurtSounds = {
         "vo/episode_1/npc/female01/cit_pain10.wav"
     },
     ["models/player/alyx.mdl"] = {
-        "npc_alyx.hurt04",
-        "npc_alyx.hurt05",
-        "npc_alyx.hurt06",
-        "npc_alyx.hurt08"
+        "*vo/npc/Alyx/hurt04.wav",
+        "*vo/npc/Alyx/hurt05.wav",
+        "*vo/npc/Alyx/hurt06.wav",
+        "*vo/npc/Alyx/hurt08.wav"
     },
     ["models/player/monk.mdl"] = {
-        "ravenholm.monk_pain01",
-        "ravenholm.monk_pain02",
-        "ravenholm.monk_pain03",
-        "ravenholm.monk_pain04",
-        "ravenholm.monk_pain05",
-        "ravenholm.monk_pain06",
-        "ravenholm.monk_pain07",
-        "ravenholm.monk_pain08",
-        "ravenholm.monk_pain09",
-        "ravenholm.monk_pain10",
-        "ravenholm.monk_pain12"
+        "*vo/ravenholm/monk_pain01.wav",
+        "*vo/ravenholm/monk_pain02.wav",
+        "*vo/ravenholm/monk_pain03.wav",
+        "*vo/ravenholm/monk_pain04.wav",
+        "*vo/ravenholm/monk_pain05.wav",
+        "*vo/ravenholm/monk_pain06.wav",
+        "*vo/ravenholm/monk_pain07.wav",
+        "*vo/ravenholm/monk_pain08.wav",
+        "*vo/ravenholm/monk_pain09.wav",
+        "*vo/ravenholm/monk_pain10.wav",
+        "*vo/ravenholm/monk_pain11.wav",
+        "*vo/ravenholm/monk_pain12.wav"
     },
     ["models/player/barney.mdl"] = {
-        "npc_barney.ba_pain01",
-        "npc_barney.ba_pain02",
-        "npc_barney.ba_pain03",
-        "npc_barney.ba_pain04",
-        "npc_barney.ba_pain05",
-        "npc_barney.ba_pain06",
-        "npc_barney.ba_pain07",
-        "npc_barney.ba_pain08",
-        "npc_barney.ba_pain09",
-        "npc_barney.ba_pain10"
+        "*vo/npc/Barney/ba_pain01.wav",
+        "*vo/npc/Barney/ba_pain02.wav",
+        "*vo/npc/Barney/ba_pain03.wav",
+        "*vo/npc/Barney/ba_pain04.wav",
+        "*vo/npc/Barney/ba_pain05.wav",
+        "*vo/npc/Barney/ba_pain06.wav",
+        "*vo/npc/Barney/ba_pain07.wav",
+        "*vo/npc/Barney/ba_pain08.wav",
+        "*vo/npc/Barney/ba_pain09.wav",
+        "*vo/npc/Barney/ba_pain10.wav"
     },
     ["models/player/eli.mdl"] = {
-        "ep_02.eli_launch_pain04",
-        "ep_02.eli_launch_pain05",
-        "ep_02.eli_launch_pain06",
-        "ep_02.eli_launch_pain09",
-        "ep_02.eli_launch_pain139"
+        "*vo/outland_12a/launch/eli_launch_pain01.wav",
+        "*vo/outland_12a/launch/eli_launch_pain04.wav",
+        "*vo/outland_12a/launch/eli_launch_pain05.wav",
+        "*vo/outland_12a/launch/eli_launch_pain06.wav",
+        "*vo/outland_12a/launch/eli_launch_pain09.wav",
+        "*vo/outland_12a/launch/eli_launch_pain13.wav"
     },
 
     --Combine
-    ["models/player/police.mdl"] = {
-        "NPC_MetroPolice.Pain"
-    },
-    ["models/player/police_fem.mdl"] = {
-        "NPC_MetroPolice.Pain"
-    },
-    ["models/player/combine_soldier.mdl"] = {
-        "NPC_MetroPolice.Pain"
-    },
-    ["models/player/combine_soldier_prisonguard.mdl"] = {
-        "NPC_MetroPolice.Pain"
-    },
-    ["models/player/combine_super_soldier.mdl"] = {
-        "NPC_MetroPolice.Pain"
-    }
+    ["models/player/police.mdl"] = combineSounds,
+    ["models/player/police_fem.mdl"] = combineSounds,
+    ["models/player/combine_soldier.mdl"] = combineSounds,
+    ["models/player/combine_soldier_prisonguard.mdl"] = combineSounds,
+    ["models/player/combine_super_soldier.mdl"] = combineSounds
 }
 
 local function genderOfPlayerModel( mdl )
@@ -89,11 +88,11 @@ local function genderOfPlayerModel( mdl )
 end
 
 hook.Remove( "EntityTakeDamage", "custom_sounds" )
-hook.Add( "EntityTakeDamage", "custom_sounds", function( vic, dmginfo )
-    if not IsValid( vic ) then return end
-    if not vic:IsPlayer() then return end
+hook.Add( "EntityTakeDamage", "custom_sounds", function( victim, dmginfo )
+    if not IsValid( victim ) then return end
+    if not victim:IsPlayer() then return end
 
-    local mdl = vic:GetModel()
+    local mdl = victim:GetModel()
     local soundTable = HurtSounds[ mdl ]
 
     if soundTable == nil then
@@ -111,6 +110,6 @@ hook.Add( "EntityTakeDamage", "custom_sounds", function( vic, dmginfo )
     if dmginfo:GetDamage() > 10 then
         local randSound = soundTable[ math.random( 1, table.Count( soundTable ) ) ]
         --local duration = SoundDuration( randSound ) TODO: Convert all sounds to their .wav counterpart
-        vic:EmitSound( randSound )
+        victim:EmitSound( randSound )
     end
 end )
